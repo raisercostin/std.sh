b74ffebf5f95edad57e6ce39621174664f1fbbdc
Mikle-Bond <Mikle_Bond@mail.ru>
2019-01-02 19:39:52 +0300

    Reimplemented test runner script
    It mostly exists to check whether one std.sh-powered script can run
    another such script.

---
+++ test/run-tests-nested.sh
@@ -0,0 +1,25 @@
+eval `std.sh`
+
+use args
+use log
+
+log_format() {
+	@args lvl msg...
+	echo ":: [test-main] [$lvl] ${msg[@]}"
+}
+
+main() {
+	@args
+
+	cd "$(dirname "${argv[0]}")"
+	for t in test-*.sh
+	do
+		log note "Running $t..."
+		./$t
+		log note "Finished $t."
+	done
+	exit 0
+}
+
+main "$@"
+
