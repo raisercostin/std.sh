01b9cdbdb5591073ef561988bf13f4d1eae58e04
Mikle-Bond <Mikle_Bond@mail.ru>
2019-02-14 14:50:10 +0300

    Return _stdsh_MACRO_COMMAND back
    Now 'require' macro should work again.

--- lib/macro.sh
+++ lib/macro.sh
@@ -1,8 +1,24 @@
 # @macro [ expr ... ] == eval "$(expr)"
 use args
 
-alias       @macro=$'eval "${BASH_COMMAND/*\\#/eval \\\"\$( } )\\\" " \#'
-alias @macro-print=$'eval "${BASH_COMMAND/*\\#/echo \\\"\$( } )\\\" " \#'
+# The var-substitute solution (without additional variable).
+# This approach is fail-prone if macro has pattern ' \#' in it.
+# Pro: simple; Con: macro-command doesn't work
+#alias       @macro=$'eval "${BASH_COMMAND/*\\\#/eval \\\"\$( } )\\\" " \#'
+#alias @macro-print=$'eval "${BASH_COMMAND/*\\\#/echo \\\"\$( } )\\\" " \#'
+
+# Copy-paste solution
+# It stores full command in variable
+# Pro: macro-command works again; Con: DRY voliation
+#alias       @macro=$'_stdsh_MACRO_COMMAND="\'${BASH_COMMAND#* \\\# }\'" eval "eval \\"\$(${BASH_COMMAND#* \\\# })\\"" \#'
+#alias @macro-print=$'_stdsh_MACRO_COMMAND="\'${BASH_COMMAND#* \\\# }\'" eval "echo \\"\$(${BASH_COMMAND#* \\\# })\\"" \#'
+
+# Dummy-variable solution
+# Variable exists, but doesn't contain command
+# Pros: macro-command works, still simple enough
+# Con: none known yet.
+alias       @macro=$'_stdsh_MACRO_COMMAND="yes" eval "eval \\"\$(${BASH_COMMAND#* \\\# })\\"" \#'
+alias @macro-print=$'_stdsh_MACRO_COMMAND="yes" eval "echo \\"\$(${BASH_COMMAND#* \\\# })\\"" \#'
 
 macro-command ()
 {
