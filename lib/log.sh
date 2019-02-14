bb312c9a67b843e96ae5e11c08e79f15cfbb8c3d
Mikle-Bond <Mikle_Bond@mail.ru>
2019-02-14 14:39:55 +0300

    Extend logging facility
    Added aliases for logging levels, additional checks on change level
    attempt.

--- lib/log.sh
+++ lib/log.sh
@@ -6,10 +6,10 @@ get_log_level() {
 	@args level
 	declare -r -A LOG_LEVELS=(
 		["alert"]="0"
-		["crit"]="1"
-		["err"]="2"
-		["warn"]="3"
-		["note"]="4"
+		["crit"]="1" ["critical"]="1"
+		["err"]="2"  ["error"]="2"
+		["warn"]="3" ["warning"]="3"
+		["note"]="4" ["notice"]="4" ["notify"]="4"
 		["info"]="5"
 		["debug"]="6"
 	)
@@ -18,14 +18,21 @@ get_log_level() {
 	elif [[ " ${LOG_LEVELS[*]} " == *" $level "* ]] ; then
 		echo "$level"
 	else
-		echo "std.sh: strange log level was requested" 1>&2
+		echo "std.sh: strange log level was requested: $level" 1>&2
 		echo $_stdsh_LOG_LEVEL
 	fi
 }
 
 log_level() {
 	@args level
-	_stdsh_LOG_LEVEL="$(get_log_level $level)"
+	local l=$(get_log_level $level)
+	if [ "$l" -gt 6 ] ; then
+		_stdsh_LOG_LEVEL=6
+	elif [ "$l" -lt 0 ] ; then
+		_stdsh_LOG_LEVEL=0
+	else
+		_stdsh_LOG_LEVEL="$l"
+	fi
 }
 
 # This function declares the format of logs.
