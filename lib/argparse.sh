5a9bad23714895c62c591149a41124c63a59f41b
Mikle-Bond <Mikle_Bond@mail.ru>
2019-02-14 14:51:34 +0300

    Add simple argument parser
    For now, it's just a shortcut to ease script's arguments parsing.

---
+++ lib/argparse.sh
@@ -0,0 +1,13 @@
+#!/bin/false
+use macro
+
+function argparse() {
+	@args name short long
+cat <<END
+	local TEMP
+	TEMP=\$(getopt -n "$name" -o "$short" -l "$long" -- "\$@") || error "Can't parse arguments"
+	eval set -- \$TEMP
+END
+}
+
+macroify argparse
